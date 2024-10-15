c2f13822bde43179df0ef73c82d8c249d2109c92
mattkatt <m4ttk4tt@hotmail.co.uk>
2024-10-15 18:47:50 +0100

    initali commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rik-Rok
+
+A fake version of a popular video sharing platform, created for Robin's birthday.
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <h1>Hello {{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const name = 'Vue 3';
+</script>
+
+<style scoped lang="postcss">
+.card {
+  @apply bg-gray-100 rounded-lg p-4 text-center;
+}
+
+.card h1 {
+  @apply text-2xl font-bold text-blue-500;
+}
+</style>
