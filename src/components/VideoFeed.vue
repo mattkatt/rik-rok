798717a13fe9d240d8257bb0796d94985ed84e55
mattkatt <m4ttk4tt@hotmail.co.uk>
2024-10-17 23:15:35 +0100

    feat: more videos. minor fixes

--- src/components/VideoFeed.vue
+++ src/components/VideoFeed.vue
@@ -1,13 +1,14 @@
 <template>
   <AppHeader :current="videos[currentVideoIndex]"/>
   <div ref="videoFeed" class="video-feed">
-    <VideoItem
-      v-for="(video, i) in videos"
-      :key="i"
-      :src="video"
-      :autoplay="i !== 0"
-      @ended="nextVideo"
-    />
+    <template v-for="(video, i) in videos" :key="i">
+      <VideoItem
+        v-if="loaded || i === 0"
+        :src="video"
+        :autoplay="i !== 0"
+        @ended="nextVideo"
+      />
+    </template>
   </div>
   <AppFooter />
 </template>
@@ -21,22 +22,30 @@ const { height } = useWindowSize()
 const videoFeed = ref(null);
 const currentVideoIndex = ref(0);
 const videoSize = ref(200)
+const loaded = ref(false);
 
 const videos = ref([
     "/rik-rok/videos/matt_evans.mp4",
     ...useShuffle([
       "/rik-rok/videos/anina_kinzel.mp4",
+      "/rik-rok/videos/carly_&_jordan.mp4",
       "/rik-rok/videos/cath_elms.mp4",
       "/rik-rok/videos/chris_nicholls.mp4",
       "/rik-rok/videos/dean_brown.mp4",
+      "/rik-rok/videos/jak_&_rose.mp4",
       "/rik-rok/videos/jess_elvin.mp4",
+      "/rik-rok/videos/keri_moriarty.mp4",
+      "/rik-rok/videos/kirsty_rowles.mp4",
       "/rik-rok/videos/liz_taylor.mp4",
       "/rik-rok/videos/lou_peck.mp4",
       "/rik-rok/videos/lucy_bason.mp4",
+      "/rik-rok/videos/melon_bailey.mp4",
       "/rik-rok/videos/morgan_clark.mp4",
       "/rik-rok/videos/nico_campbell.mp4",
+      "/rik-rok/videos/ruth_pallister.mp4",
       "/rik-rok/videos/ste_hough.mp4",
       "/rik-rok/videos/tim_hunt.mp4",
+      "/rik-rok/videos/vikki_baker.mp4",
     ]),
   "/rik-rok/videos/rick_astley.mp4",
 ]);
@@ -63,8 +72,9 @@ const { isSwiping } = useSwipe(videoFeed, {
 });
 
 onMounted(() => {
-  videoSize.value = videoFeed.value.clientHeight / videos.value.length;
+  videoSize.value = videoFeed.value.clientHeight;
   videoFeed.value.style.transform = `translate3d(0, -${(currentVideoIndex.value) * videoSize.value}px, 0)`;
+  loaded.value = true;
 });
 
 watch(currentVideoIndex, () => {
